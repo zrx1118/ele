208c8510291860e6159798179ca237665e237fc6
zrx <380429100@qq.com>
2017-09-25 22:06:20 +0800

    components all

---
+++ src/components/Order.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="order">
+        <h1>{{msg}}</h1>
+    </div>
+</template>
+    
+<script>
+export default {
+  name: "order",
+  data () {
+    return {
+        msg:'order'
+    };
+  }
+}
+</script>
+    
+<style lang="css" scoped>
+
+</style>
